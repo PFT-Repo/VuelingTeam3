3ed64e7202d2949856886f2ea9d30756581bff0b
HenryArtR <githubworkflowhenry@gmail.com>
2023-02-21 13:30:01 +0100

    json, interface y service de list_of_flights

--- VuelingTechHack/FrontEnd/projects/data/src/lib/services/test.service.ts
+++ VuelingTechHack/FrontEnd/projects/data/src/lib/services/test.service.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ListOfFLights } from 'src/app/interfaces/listOfFlights.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TestService {
-  constructor() {}
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getListOfFlights(){
+    return this.http.get<ListOfFLights>('')
+  }
+
+  
 }
---
+++ VuelingTechHack/FrontEnd/src/app/interfaces/listOfFlights.interface.ts
@@ -0,0 +1,12 @@
+export interface ListOfFLights {
+  day: string,
+  hour: string,
+  handlingFunction: string,
+  fullTimeEmployers: string,
+  partTimeEmployeers: string,
+  totalEmployers: string,
+  fullTimeEmployersCost: string,
+  partTimeEmployeersCost: string,
+  totalCost: string,
+  
+}
