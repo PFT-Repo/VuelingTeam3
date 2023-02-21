ab3c60578c53de7c5d252c07671c678ab647eca8
TurpialCode <programasfulltech@gmail.com>
2023-02-21 12:52:31 +0100

    fixing theme

--- VuelingTechHack/FrontEnd/src/app/public/public.module.ts
+++ VuelingTechHack/FrontEnd/src/app/public/public.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
+import { LoginComponent } from './views/login/login.component';
+import {MatInputModule} from '@angular/material/input'
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, PublicRoutingModule],
+  declarations: [
+    LoginComponent
+  ],
+  imports: [CommonModule, PublicRoutingModule,MatInputModule],
 })
 export class PublicModule {}
