cc2198f24c1ef2e604818accad192036c2341308
Rodolfo Rosales Estrada <rodolfo.rosales@col.vueling.com>
2023-02-21 17:05:32 +0100

    employees by function

--- VuelingTechHack/FrontEnd/src/app/public/material.module.ts
+++ VuelingTechHack/FrontEnd/src/app/public/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 
-const MATERIAL_MODULES = [MatInputModule,MatButtonModule];
+const MATERIAL_MODULES = [MatInputModule, MatButtonModule];
 
 @NgModule({
   imports: MATERIAL_MODULES,
--- VuelingTechHack/FrontEnd/src/app/public/views/dashboard/dashboard.component.ts
+++ VuelingTechHack/FrontEnd/src/app/public/views/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeesByFunctionConfig } from '../../components';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  employeesByFunction = [12, 19, 3, 5, 2, 3];
+  employeesByFunction: EmployeesByFunctionConfig = {
+    title: 'Empleados por función',
+    labels: ['Jardinera', 'Equipaje', 'Coordinación'],
+    data: [12, 25, 2],
+    datasetLabel: 'Total de Empleados',
+  };
 }
