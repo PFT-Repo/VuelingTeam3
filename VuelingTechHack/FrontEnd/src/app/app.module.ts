ff2adca97bb743a13ab6f3124a45a2249e50f6ad
TurpialCode <programasfulltech@gmail.com>
2023-02-21 11:53:52 +0100

    first with material

--- VuelingTechHack/FrontEnd/src/app/app.module.ts
+++ VuelingTechHack/FrontEnd/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpipePipe } from './pipes/mainpipe.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MainpipePipe } from './pipes/mainpipe.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
