0e19febbaf917e724c0560680ed2eeec409a1ec9
paucala <pau.calabres@gmail.com>
2023-02-21 13:16:21 +0100

    basics models data for front-end

--- VuelingTechHack/BackEnd/src/main/java/team3/backend/Team3BackEnd/repository/GHOrepository.java
+++ VuelingTechHack/BackEnd/src/main/java/team3/backend/Team3BackEnd/repository/GHOrepository.java
@@ -1,4 +1,8 @@
 package team3.backend.Team3BackEnd.repository;
 
-public interface GHOrepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import team3.backend.Team3BackEnd.model.in.GroundHandlingInput;
+
+public interface GHOrepository extends MongoRepository<GroundHandlingInput, String> {
 }
