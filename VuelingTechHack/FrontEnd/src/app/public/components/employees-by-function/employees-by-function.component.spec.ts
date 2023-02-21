25090da52bc94122af281cf59a555cfebbf0a381
Rodolfo Rosales Estrada <rodolfo.rosales@col.vueling.com>
2023-02-21 16:08:04 +0100

    charts v1
    # Conflicts:
    #	VuelingTechHack/FrontEnd/src/app/public/public.module.ts

---
+++ VuelingTechHack/FrontEnd/src/app/public/components/employees-by-function/employees-by-function.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeesByFunctionComponent } from './employees-by-function.component';
+
+describe('EmployeesByFunctionComponent', () => {
+  let component: EmployeesByFunctionComponent;
+  let fixture: ComponentFixture<EmployeesByFunctionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmployeesByFunctionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EmployeesByFunctionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- VuelingTechHack/FrontEnd/src/app/public/public.module.ts
+++ VuelingTechHack/FrontEnd/src/app/public/public.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
-import { LoginComponent } from './views/login/login.component';
-import { TableComponent } from './components/table/table.component';
-import {MatInputModule} from '@angular/material/input'
-import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent, DashboardComponent } from './views';
 import { MaterialModule } from './material.module';
+import {
+  ChartComponent,
+  EmployeesByFunctionComponent,
+  TableComponent,
+} from './components';
 
 @NgModule({
-  declarations: [LoginComponent, DashboardComponent,TableComponent],
-  exports: [TableComponent],
-  imports: [CommonModule, PublicRoutingModule, MaterialModule,MatInputModule,MatButtonModule],
+  declarations: [
+    LoginComponent,
+    ChartComponent,
+    TableComponent,
+    DashboardComponent,
+    EmployeesByFunctionComponent,
+  ],
+  imports: [
+    CommonModule,
+    PublicRoutingModule,
+    MaterialModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class PublicModule {}
