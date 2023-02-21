8f14f5bd632149440b53c3353059e29a4df10516
HenryArtR <githubworkflowhenry@gmail.com>
2023-02-21 15:37:48 +0100

    change route

--- VuelingTechHack/FrontEnd/src/app/public/public-routing.module.ts
+++ VuelingTechHack/FrontEnd/src/app/public/public-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
-  { path: '/', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   {
     path: '**',
     redirectTo: '/',
