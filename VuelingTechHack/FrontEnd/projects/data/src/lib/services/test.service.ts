8818c21d00cddc66fd2fae14f6d431e832681a5f
HenryArtR <githubworkflowhenry@gmail.com>
2023-02-21 13:41:41 +0100

    environmet y fakejson hechos

--- VuelingTechHack/FrontEnd/projects/data/src/lib/services/test.service.ts
+++ VuelingTechHack/FrontEnd/projects/data/src/lib/services/test.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
 import { ListOfFLights } from 'src/app/interfaces/listOfFlights.interface';
 
 @Injectable({
@@ -11,7 +12,7 @@ export class TestService {
   ) {}
 
   getListOfFlights(){
-    return this.http.get<ListOfFLights>('')
+    return this.http.get<ListOfFLights>(environment.apiUrl)
   }
 
   
--- VuelingTechHack/FrontEnd/src/app/public/public.module.ts
+++ VuelingTechHack/FrontEnd/src/app/public/public.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { LoginComponent } from './views/login/login.component';
-import {MatInputModule} from '@angular/material/input'
+import {MatInputModule} from '@angular/material/input';
+import { TableComponent } from './components/table/table.component'
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    TableComponent
   ],
   imports: [CommonModule, PublicRoutingModule,MatInputModule],
 })
