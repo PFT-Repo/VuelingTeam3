dc6a8dc253c5e04cede07683fad52647562a96f5
HenryArtR <githubworkflowhenry@gmail.com>
2023-02-21 12:14:38 +0100

    interface y service

---
+++ VuelingTechHack/FrontEnd/src/app/interfaces/lilstOfFlight.interface.ts
@@ -0,0 +1,13 @@
+export interface ListOfFLights {
+  day: string,
+  hour: string,
+  handlingFunction: string,
+  fullTimeEmployers: string,
+  partTimeEmployeers: string,
+  totalEmployers: string,
+  fullTimeEmployersCost: string,
+  partTimeEmployeersCost: string,
+  totalCost: string,
+  
+
+}
--- VuelingTechHack/FrontEnd/src/app/services/mainservice.service.ts
+++ VuelingTechHack/FrontEnd/src/app/services/mainservice.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ListOfFLights } from '../interfaces/lilstOfFlight.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainserviceService {
 
-  constructor() { }
+  
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  getListOfFlights(){
+    return this.http.get<ListOfFLights>('')
+  }
+
 }
